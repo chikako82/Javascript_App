5c740da901230fea15951baf534b6062df69eb65
chikako82 <chikakotakemura82@gmail.com>
2022-12-08 17:48:07 +0900

    写真入れ替え、ページ番号追加

--- JS_SlideShow/main.js
+++ JS_SlideShow/main.js
@@ -1,14 +1,21 @@
 'use strict';
 
-const image = ['img/comida1.JPG', 'img/comida2.JPG', 'img/comida3.JPG', 'img/comida4.jpg', 'img/comida5.jpg', 'img/comida6.JPG', 'img/comida7.JPG', 'img/comida8.JPG', 'img/comida9.JPG', 'img/comida10.JPG', 'img/comida11.JPG', 'img/comida12.JPG', 'img/comida13.jpg'];
+const image = ['img/comida1.JPG', 'img/comida2.JPG', 'img/comida3.JPG', 'img/comida4.jpg', 'img/comida5.jpg', 'img/comida6.JPG', 'img/comida7.JPG', 'img/comida8.JPG', 'img/comida9.JPG', 'img/comida10.JPG', 'img/comida11.JPG', 'img/comida12.JPG'];
 let current = 0;
 
 function slideShow(num) {
     if(current + num >= 0 && current + num < image.length) {
         current += num;
         document.getElementById('main_image').src = image[current];
+        page();
     }
 };
+
+function page() {
+    document.getElementById('page').textContent = `${current + 1}/${image.length}`;
+}
+
+page();
 
 document.getElementById('prev').onclick = function() {
     slideShow(-1);
